499ad67c50ab63502661f3228ab257c50ccc6b17
Yurii Pochapskiy <pochapskiy1983@gmail.com>
2025-02-17 16:27:38 +0200

    add: moving slider with dots

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const intervalSetting = 3;
+
 const arrowRight = document.querySelector('.arrow-right');
 const arrowLeft = document.querySelector('.arrow-left');
 const slider = document.querySelector('.slider');
@@ -6,8 +8,9 @@ const sliderContainer = document.querySelector('.slider-container');
 const leftArrowWrapper = document.querySelector('.left-arrow-wrapper');
 
 
-
-let currentStateX = 0
+let interval;
+let currentStateX = 0;
+let isMovingRight = true;
 const carouselImageLength = carouselImage.length;
 const carouselImageWidth = carouselImage[0].getBoundingClientRect().width;
 const arrowContainerWidth = 10;
@@ -15,29 +18,89 @@ const arrowContainerWidth = 10;
 const rightArrowWidth = arrowRight.getBoundingClientRect().width + arrowContainerWidth;
 const leftArrowWidth = arrowLeft.getBoundingClientRect().width + arrowContainerWidth;
 const arrowsWidth = rightArrowWidth + leftArrowWidth;
-
 sliderContainer.style.maxWidth = `${carouselImageWidth + arrowsWidth}px`;
 console.log(leftArrowWidth, rightArrowWidth, arrowsWidth);
 
-arrowRight.addEventListener('click', function () {
+
+
+arrowRight.addEventListener('click', arrowRightClick);
+arrowLeft.addEventListener('click', arrowLeftClick);
+sliderContainer.addEventListener('mouseenter', stopSlider);
+sliderContainer.addEventListener('mouseleave', startSlider);
+const sliderDotsContainer = document.querySelector('.slider-dots-container');
+
+
+
+generateDots();
+updateDots();
+startSlider();
+
+function updateSlider() {
+  slider.style.transform = `translateX(${-currentStateX * carouselImageWidth}px)`;
+  updateDots();
+}
+
+function arrowRightClick() {
   currentStateX++;
   console.log('Right: ', carouselImageWidth);
   if (currentStateX >= carouselImageLength) {
     currentStateX = 0;
   }
-  slider.style.transform = `translateX(${-currentStateX * carouselImageWidth}px)`;
-});
+  updateSlider();
+}
 
-
-arrowLeft.addEventListener('click', function () {
+function arrowLeftClick() {
   currentStateX--;
-  console.log('Left: ', carouselImageWidth)
+  console.log('Left: ', carouselImageWidth, (new Date()).getTime())
   if (currentStateX < 0) {
     currentStateX = carouselImageLength - 1;
   }
-  slider.style.transform = `translateX(${-currentStateX * 100}%)`;
-});
+  updateSlider();
+}
+
+function startSlider() {
+  interval = setInterval(() => {
+    if (
+      (isMovingRight && currentStateX === carouselImageLength - 1) ||
+      (!isMovingRight && currentStateX === 0)
+    ) {
+      isMovingRight = !isMovingRight;
+    }
+
+    if (isMovingRight) {
+      arrowRightClick();
+    } else {
+      arrowLeftClick();
+    }
+  }, intervalSetting * 1000);
+}
+
+function stopSlider() {
+  clearInterval(interval);
+  console.log('Остановка слайдера');
+}
+
+function generateDots() {
+  sliderDotsContainer.innerHTML = '';
+  for (let i = 0; i < carouselImageLength; i++) {
+    const dot = document.createElement('div');
+    dot.classList.add('slide-dot');
+    dot.dataset.index = i;
+    dot.addEventListener('click', () => {
+      currentStateX = Number(dot.dataset.index);
+      updateSlider();
+      updateDots();
+    });
+    sliderDotsContainer.appendChild(dot);
+  }
+}
 
 
-const rightContainer = document.querySelector('.right-arrow-container').getBoundingClientRect().width;
-console.log(rightContainer);
+function updateDots() {
+  const dots = sliderDotsContainer.querySelectorAll('.slide-dot');
+  dots.forEach(dot => dot.classList.remove('active'));
+  const activeDot = sliderDotsContainer.querySelector(`.slide-dot[data-index="${currentStateX}"]`);
+  if (activeDot) {
+    activeDot.classList.add('active');
+  }
+}
